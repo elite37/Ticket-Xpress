bd599144b507022b2b8b6c81fd8a0c048ae6969b
Alabi Abayomi <abayomi15@gmail.com>
2021-10-06 11:29:38 +0100

    fixed sign up bugs and added agent dashboard

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,20 +3,21 @@ import { login } from './auth.js'
 let error = document.getElementById('authError')
 let loginButton = document.getElementById('loginButton')
 let emailField = document.getElementById('emailField')
-let firstNameField = document.getElementById('firstNameField')
-let lastNameField = document.getElementById('lastNameField')
+// let firstNameField = document.getElementById('firstNameField')
+// let lastNameField = document.getElementById('lastNameField')
 let passwordField = document.getElementById('passwordField')
 
 loginButton.addEventListener('click', (e)=>{
     e.preventDefault()
-    console.log('se')
+    // console.log('se')
     let email = emailField.value
     let password = passwordField.value
     let res = login(email, password)
-    console.log(res)
+    // console.log(res)
     if (res.error) {
         error.innerText = res.error
+    } else {
+    // alert('Logged in')
+    window.location.pathname = '/pages/userdashboard.html'
     }
-    alert('Logged in')
-    // window.location.pathname = '/pages/userdashboard.html'
 })
