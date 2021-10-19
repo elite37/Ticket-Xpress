744fd8cabfbce92bf1aa1c2bf1758baa0c1e461b
Adetayo Omotomiwa <adetayoomotomiwa99@gmail.com>
2021-10-19 14:02:26 +0100

    Working On The Mobile Responsiveness

--- assets/js/headerFixed.js
+++ assets/js/headerFixed.js
@@ -1,7 +1,9 @@
 
 
       const header = document.querySelector("header");
-
+      const burger = document.querySelector(".nav-opener");
+      const navTarget = document.querySelector(".nav__target");
+      const navContainer = document.querySelector(".nav__container");
 
       window.addEventListener("scroll" , () => {
 
@@ -19,4 +21,25 @@
       })
 
 
-console.log(header)
+   burger.addEventListener('click' , () => {
+
+            const burgerIcon = burger.querySelector("i");
+          
+                  burgerIcon.classList.toggle('fa-times');
+                navTarget.classList.toggle("open");             
+               navContainer.classList.toggle("open");
+   })
+
+
+    window.addEventListener("click" , (e) => {
+       
+        if(e.target === navTarget) {
+            const burgerIcon = burger.querySelector("i");
+          
+            burgerIcon.classList.toggle('fa-times');
+          navTarget.classList.toggle("open");             
+         navContainer.classList.toggle("open");     
+        }
+
+
+    })
