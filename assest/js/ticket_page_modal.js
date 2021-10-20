b002792351d4a18a37276eb9d000dfe5f1a2a8bb
Damilola <dahmie@gmail.com>
2021-10-20 22:14:49 +0100

    ticket page

--- assest/js/ticket_page_modal.js
+++ assest/js/ticket_page_modal.js
@@ -1,6 +1,6 @@
-function showModal() {
+function showModal1() {
     console.log("working");
-    const modalDisplay = document.getElementById("modal-display");
+    const modalDisplay = document.getElementById("modal-display1");
     
     modalDisplay.style.display = "none";
     if(modalDisplay.style.display === "block") {
@@ -10,8 +10,8 @@ function showModal() {
     }
 }
 
-function closeModal() {
-    const modalDisplay = document.getElementById("modal-display");
+function closeModal1() {
+    const modalDisplay = document.getElementById("modal-display1");
     
     // modalDisplay.style.display = "none";
     if(modalDisplay.style.display === "block") {
@@ -22,7 +22,56 @@ function closeModal() {
 
 }
 
-function submitModal() {
+function submitModal1() {
+    const firstName = document.querySelector('.firstName');
+    const lastName = document.querySelector('.lastName');
+    const email = document.querySelector('.email');
+
+    let bookingObj = {
+        firstName : firstName.value,
+        lastName : lastName.value,
+        email : email.value
+    }
+      console.log(bookingObj)
+    let bookingData = JSON.parse(localStorage.getItem("bookingData"));
+    //  let bookingArray
+
+    if(!bookingData) {
+       bookingData = []
+    }
+    bookingData.push(bookingObj)
+    console.log(bookingData);
+    
+    localStorage.setItem("bookingData", JSON.stringify(bookingData))
+    closeModal1()
+}
+
+
+function showModal2() {
+    console.log("working");
+    const modalDisplay = document.getElementById("modal-display2");
+    
+    modalDisplay.style.display = "none";
+    if(modalDisplay.style.display === "block") {
+        modalDisplay.style.display = "none";
+    } else {
+        modalDisplay.style.display = "block";
+    }
+}
+
+function closeModal2() {
+    const modalDisplay = document.getElementById("modal-display2");
+    
+    // modalDisplay.style.display = "none";
+    if(modalDisplay.style.display === "block") {
+        modalDisplay.style.display = "none";
+    } else {
+        modalDisplay.style.display = "block";
+    }
+
+}
+
+function submitModal2() {
     const bookNumber = document.querySelector('.bookingNum');
     const lastName = document.querySelector('.lastName');
     const email = document.querySelector('.email');
@@ -43,5 +92,53 @@ function submitModal() {
     console.log(bookingData);
     
     localStorage.setItem("bookingData", JSON.stringify(bookingData))
-    closeModal()
+    closeModal2()
+}
+
+function showModal3() {
+    console.log("working");
+    const modalDisplay = document.getElementById("modal-display3");
+    
+    modalDisplay.style.display = "none";
+    if(modalDisplay.style.display === "block") {
+        modalDisplay.style.display = "none";
+    } else {
+        modalDisplay.style.display = "block";
+    }
+}
+
+function closeModal3() {
+    const modalDisplay = document.getElementById("modal-display3");
+    
+    // modalDisplay.style.display = "none";
+    if(modalDisplay.style.display === "block") {
+        modalDisplay.style.display = "none";
+    } else {
+        modalDisplay.style.display = "block";
+    }
+
+}
+
+function submitModal3() {
+    const bookNumber = document.querySelector('.bookingNum');
+    const lastName = document.querySelector('.lastName');
+    const email = document.querySelector('.email');
+
+    let bookingObj = {
+        bookNumber : bookNumber.value,
+        lastName : lastName.value,
+        email : email.value
+    }
+      console.log(bookingObj)
+    let bookingData = JSON.parse(localStorage.getItem("bookingData"));
+    //  let bookingArray
+
+    if(!bookingData) {
+       bookingData = []
+    }
+    bookingData.push(bookingObj)
+    console.log(bookingData);
+    
+    localStorage.setItem("bookingData", JSON.stringify(bookingData))
+    closeModal3()
 }
